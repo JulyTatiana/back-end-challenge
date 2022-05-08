5e4f7142e31d362bc2d928f0a83ffcf4d99d9eec
julytatiana <yulytatiana@live.com>
2022-05-07 23:42:32 -0500

    first commit

---
+++ src/main/java/com/sofkau/webdevelopment/todoapp/RegisterApp.java
@@ -0,0 +1,13 @@
+package com.sofkau.webdevelopment.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterApp.class, args);
+	}
+
+}
